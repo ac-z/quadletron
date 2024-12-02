e4c78c8dabf9caa6c0ccdbf71b55b358ca984b3d
Amber Connelly <113668892+ac-z@users.noreply.github.com>
2024-12-02 09:55:59 -0800

    Initial commit

---
+++ profiledef.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="quadletron"
+iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Not for distribution"
+iso_application="Archiso Quadletron"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito'
+           'bios.syslinux.mbr' 'bios.syslinux.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking')
+bootstrap_tarball_compression=(zstd -c -T0 --long -19)
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+)
